c49dd484b62fb4a50b976d3c2532d5e8d74fb0e0
drudkovsky <Marckys2013@gmail.com>
2015-12-05 22:32:59 +0300

    Budget v1.0

--- Budget — control working version/app/src/main/java/com/example/android/budget/MainActivity.java
+++ Budget — control working version/app/src/main/java/com/example/android/budget/MainActivity.java
@@ -18,8 +18,6 @@ public class MainActivity extends AppCompatActivity {
     int summValue;
     int interedNumber;
     private SharedPreferences Budget;
-    
-
 
 
 
@@ -31,33 +29,75 @@ public class MainActivity extends AppCompatActivity {
         Budget = getSharedPreferences("MyData", 0);
     }
 
-    @Override
-    protected void onPause() {
-        super.onPause();
-        // Запоминаем данные
-        SharedPreferences.Editor editor = Budget.edit();
-        editor.putInt("MyData", summValue);
-        editor.apply();
+    // Method for displaying the number
+    private void display(int number) {
+        TextView summaryId = (TextView) findViewById(R.id.Sunnury_id);
+        summaryId.setText(number + "РУБ");
     }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-
-        if (Budget.contains("MyData")) {
-            // Получаем число из настроек
-            summValue = Budget.getInt("summValue", summValue);
-            // Надо вывести на экран
-            TextView summaryId = (TextView) findViewById(R.id.Sunnury_id);
-            summaryId.setText(summValue + "РУБ");
+    //Method for grabbing the intered number from edittext to the variable
+    private int GrabbingNumber() {
+        TextView UserNumber = (TextView) findViewById(R.id.Entered_number_id);
+        String CheckInteredNumber = UserNumber.getText().toString();
+        if (UserNumber.getText().toString().equals("")) {
+            interedNumber = 0;
+            Toast.makeText(getApplicationContext(), "Введите сумму!",
+                    Toast.LENGTH_LONG).show();
+            return interedNumber;
+        } else {
+            interedNumber = Integer.parseInt(UserNumber.getText().toString());
+            return interedNumber;
         }
     }
 
-    // Method for displaying the number
-    private void display(int number) {
+    ;
 
-        TextView summaryId = (TextView) findViewById(R.id.Sunnury_id);
-        summaryId.setText(number + "РУБ");
+    // Method for adding intered number to SummValue and displaying the number
+    public void Adding(View view) {
+
+        GrabbingNumber();
+        summValue = summValue + interedNumber;
+        display(summValue);
+        ImageView pic = (ImageView) findViewById(R.id.Pic_id);
+        if (summValue >= 20000) {
+            pic.setImageResource(R.drawable.alotmoney);
+        }
+        if ((summValue >= 10000) & (summValue < 20000)) {
+            pic.setImageResource(R.drawable.money);
+        }
+        if (summValue >= 5000 & summValue < 10000) {
+            pic.setImageResource(R.drawable.somemoney);
+        }
+        if (summValue >= 3000 & summValue < 5000) {
+            pic.setImageResource(R.drawable.littlemoney);
+
+        }
+        if (summValue < 3000) {
+            pic.setImageResource(R.drawable.nomoney);
+        }
+        if (summValue < 0){
+            TextView dept = (TextView) findViewById(R.id.Sunnury_id);
+            dept.setTextColor(Color.parseColor("#F44336"));
+            pic.setImageResource(R.drawable.overbudget);
+        }
+        if (summValue >= 0){
+            TextView dept = (TextView) findViewById(R.id.Sunnury_id);
+            dept.setTextColor(Color.parseColor("#000000"));
+
+        }
+
+        SharedPreferences.Editor editor = Budget.edit();
+        editor.putInt("summValue", summValue);
+        editor.commit();
+
+
+    }
+
+    // Method for substruction intered number to SummValue and displaying the number
+    public void Substraction(View view) {
+        GrabbingNumber();
+        summValue = summValue - interedNumber;
+        display(summValue);
         ImageView pic = (ImageView) findViewById(R.id.Pic_id);
         if (summValue >= 20000) {
             pic.setImageResource(R.drawable.alotmoney);
@@ -84,46 +124,10 @@ public class MainActivity extends AppCompatActivity {
             TextView dept = (TextView) findViewById(R.id.Sunnury_id);
             dept.setTextColor(Color.parseColor("#000000"));
         }
-    }
 
-    //Method for grabbing the intered number from edittext to the variable
-    private int GrabbingNumber() {
-        TextView UserNumber = (TextView) findViewById(R.id.Entered_number_id);
-        String CheckInteredNumber = UserNumber.getText().toString();
-        if (UserNumber.getText().toString().equals("")) {
-            interedNumber = 0;
-            Toast.makeText(getApplicationContext(), "Введите сумму!",
-                    Toast.LENGTH_LONG).show();
-            return interedNumber;
-        } else {
-            interedNumber = Integer.parseInt(UserNumber.getText().toString());
-            return interedNumber;
-        }
-    }
-
-    ;
-
-    // Method for adding intered number to SummValue and displaying the number
-    public void Adding(View view) {
-
-        GrabbingNumber();
-        summValue = summValue + interedNumber;
         SharedPreferences.Editor editor = Budget.edit();
         editor.putInt("summValue", summValue);
         editor.commit();
-        display(summValue);
-
-
-    }
-
-    // Method for substruction intered number to SummValue and displaying the number
-    public void Substraction(View view) {
-        GrabbingNumber();
-        summValue = summValue - interedNumber;
-        SharedPreferences.Editor editor = Budget.edit();
-        editor.putInt("summValue", summValue);
-        editor.commit();
-        display(summValue);
     }
 }
 
